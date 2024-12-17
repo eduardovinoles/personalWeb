5c79556fb3af7e465f17a38fc629ef94e56c4324
evinolesbootia <evinoles@bootia.dev>
2024-12-17 15:18:09 -0300

    Fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import { useState } from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Footer from "./components/design/Footer";
 import { ContactMe } from "./components/design/Contact";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Hero /> <Benefits />
-              </>
-            }
-          />
-          <Route path="*" element={<div>404 - Page Not Found</div>} />
-        </Routes>
+        <Hero />
+        <Benefits />
+        <ContactMe />
         <Footer />
       </div>
       <ButtonGradient />
