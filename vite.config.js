be009b72952a2ec3ac4a694797086ea532979dbe
evinolesbootia <evinoles@bootia.dev>
2024-12-19 13:18:59 -0300

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/personalWeb/",
   plugins: [react()],
   build: {
-    outDir: "dist", // Output directory
-    assetsDir: "assets", // Directory for assets
+    outDir: "dist", // Make sure the output directory is correctly set to 'dist'
     rollupOptions: {
-      input: "src/main.jsx", // Correct path to the entry point
+      input: "index.html", // Make sure the entry point is correctly defined
     },
   },
 });
