e6d1c3bcd3227e0e04317641268acb85cbd90723
evinolesbootia <evinoles@bootia.dev>
2024-12-19 16:25:04 -0300

    wip

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Use relative paths
   build: {
-    outDir: "dist", // Make sure the output directory is correctly set to 'dist'
-    rollupOptions: {
-      input: "index.html", // Make sure the entry point is correctly defined
-    },
+    outDir: "dist",
   },
 });
