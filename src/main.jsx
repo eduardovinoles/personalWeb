10eb136386ff0f70e2debe704ad4a0d39731d6e4
evinolesbootia <evinoles@bootia.dev>
2024-12-19 16:32:51 -0300

    wip

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,9 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <Router basename="/personalWeb">
-      {" "}
-      {/* Add basename prop */}
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
